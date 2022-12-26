3717cb77d6aeca199b951db7160f73fbe7428cdb
tuhoang565 <hoangcongtu565@gmail.com>
2022-12-26 21:33:25 +0700

    finish project

---
+++ securitymodule/src/main/java/security/data/SecurityRepository.java
@@ -0,0 +1,21 @@
+package security.data;
+
+import security.model.AlarmStatus;
+import security.model.ArmingStatus;
+import security.model.Sensor;
+
+import java.util.Set;
+
+/**
+ * Interface showing the methods our security repository will need to support
+ */
+public interface SecurityRepository {
+    void addSensor(Sensor sensor);
+    void removeSensor(Sensor sensor);
+    void updateSensor(Sensor sensor);
+    void setAlarmStatus(AlarmStatus alarmStatus);
+    void setArmingStatus(ArmingStatus armingStatus);
+    Set<Sensor> getSensors();
+    AlarmStatus getAlarmStatus();
+    ArmingStatus getArmingStatus();
+}
---
+++ securitymodule/src/main/java/security/model/SensorType.java
@@ -0,0 +1,8 @@
+package security.model;
+
+/**
+ * List of available sensor types. Not currently used by system, other than for display.
+ */
+public enum SensorType {
+    DOOR, WINDOW, MOTION
+}
